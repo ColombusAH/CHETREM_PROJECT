7fd55aeff96244f5477ad70630d349098fcd3404
ColombusAH <Hattabavner@gmail.com>
2019-09-24 00:16:32 +0300

    Auth Repository implemented

--- Chetrem.Api/Data/AuthRepository.cs
+++ Chetrem.Api/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Chetrem.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chetrem.Api.Data
 {
@@ -15,7 +16,35 @@ namespace Chetrem.Api.Data
         }
         public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                var success = computedHash.Length == passwordHash.Length;
+
+                for (int i = 0; i < computedHash.Length && success; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        success = false;
+                    }
+                }
+                return success;
+            }
         }
 
         public async Task<User> Register(User user, string password)
@@ -26,24 +55,25 @@ namespace Chetrem.Api.Data
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
-
-
-           
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512()) 
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
-                
+
         }
 
-        public Task<bool> UserExist(string username)
+        public async Task<bool> UserExist(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(u => u.Username == username))
+                return true;
+
+            return false;
+
         }
     }
 }
